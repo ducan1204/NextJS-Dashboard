31f5472b803f2149e724ebf95a2ad44b73a5454c
Lê Đức An <44047530+ducan1204@users.noreply.github.com>
2024-11-12 23:27:05 +0700

    Fix type error

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -12,7 +12,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
   const allPages = generatePagination(currentPage, totalPages);
-  const createPageURL = (page: number) => {
+  const createPageURL = (page: number | string) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', page.toString());
     return `${pathname}?${params.toString()}`;
