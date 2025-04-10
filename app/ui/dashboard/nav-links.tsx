5ef2742f71033f106673855497b198946342d77c
anld124 <anld@suga.com.vn>
2025-04-10 17:23:54 +0700

    Fix nav link

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,15 +1,23 @@
-'use client';
-import {HomeIcon, CheckIcon, MapIcon} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
+"use client";
+import {
+  CheckIcon,
+  HomeIcon,
+  MapIcon,
+} from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  { name: 'Words', href: '/dashboard/words', icon: CheckIcon },
-  { name: 'Maps', href: 'dashboard/maps', icon: MapIcon },
+  { name: "Home", href: "/dashboard", icon: HomeIcon },
+  {
+    name: "Words",
+    href: "/dashboard/words",
+    icon: CheckIcon,
+  },
+  { name: "Maps", href: "/dashboard/maps", icon: MapIcon },
 ];
 
 export default function NavLinks() {
@@ -23,10 +31,11 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
-              },
+                "bg-sky-100 text-blue-600":
+                  pathname === link.href,
+              }
             )}
           >
             <LinkIcon className="w-6" />
